4d1c384184751e8b0478c8f8fcf21cf1d2acf428
chaerin <achrvvv@gmail.com>
2023-03-14 16:31:38 +0900

    feat: implement marked calendar

---
+++ src/components/calendar/Calendar.style.tsx
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled'
+
+export const CalendarWrapper = styled.div`
+  .highlight {
+    background: red;
+  }
+`
--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -1,18 +1,20 @@
 import Calendar from 'react-calendar'
 import 'react-calendar/dist/Calendar.css'
+import * as S from './Calendar.style'
 
 const markDate = (date: Date) => {
-  const today = new Date()
-  if (today.getDate() === date.getDate()) {
+  // TODO: selected date array
+  const markedDate = new Date('2023-03-08')
+  if (markedDate.getDate() === date.getDate()) {
     return 'highlight'
   }
 }
 
 const DiaryCalendar = () => {
   return (
-    <div>
+    <S.CalendarWrapper>
       <Calendar tileClassName={({ date }) => markDate(date) || 's'} />
-    </div>
+    </S.CalendarWrapper>
   )
 }
 
