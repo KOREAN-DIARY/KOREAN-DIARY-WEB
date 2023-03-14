a43423fd8dfa8b9dc8a056bb40e74dfc35bda048
chaerin <achrvvv@gmail.com>
2023-03-14 16:58:47 +0900

    feat: add page route

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Diary from 'pages/Diary'
+import Form from 'pages/Form'
+import Login from 'pages/Login'
+import Main from 'pages/Main'
+import MyPage from 'pages/MyPage'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main />,
+  },
+  {
+    path: '/form',
+    element: <Form />,
+  },
+  {
+    path: '/my',
+    element: <MyPage />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/diary/:id',
+    element: <Diary />,
+  },
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
+    <RouterProvider router={router} />
     <App />
   </React.StrictMode>
 )
---
+++ src/pages/Main.tsx
@@ -0,0 +1,11 @@
+import Calendar from 'components/calendar/Calendar'
+
+const Main = () => {
+  return (
+    <div>
+      <Calendar />
+    </div>
+  )
+}
+
+export default Main
