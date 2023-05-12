77e910864f9664fad9243e968d8f320bd46add2a
chaerin <achrvvv@gmail.com>
2023-05-13 01:51:45 +0900

    fix: fix compare date

--- src/components/main/calendar/Calendar.tsx
+++ src/components/main/calendar/Calendar.tsx
@@ -1,9 +1,10 @@
 import Calendar from 'react-calendar'
 import * as S from './Calendar.style'
+import isEqual from 'date-fns/isEqual'
 
 const markDate = (date: Date) => {
-  const markedDate = new Date('2023-05-02')
-  if (markedDate === date) {
+  const markedDate = new Date('2023-05-02T00:00:00')
+  if (isEqual(markedDate, date)) {
     return 'highlight'
   }
 }
