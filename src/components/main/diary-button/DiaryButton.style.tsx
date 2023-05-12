ac8eb9e957d88fe012262262cbbff6617352be65
SoleiI <kmssun0524@sookmyung.ac.kr>
2023-05-12 22:03:25 +0900

    feat: markup diary button

---
+++ src/components/main/diary-button/DiaryButton.style.tsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled'
+
+export const DiaryButton = styled.button`
+  margin-top: 0.2rem;
+  width: 14rem;
+  height: 3.8rem;
+  border-radius: 1rem;
+  background-color: var(--white);
+  color: var(--dark-grey);
+  text-align: center;
+  font-size: 1rem;
+  font-weight: bold;
+`
