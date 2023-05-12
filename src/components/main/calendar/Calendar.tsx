60070753f5f8e747d8dd88916664a4524e99271c
chaerin <achrvvv@gmail.com>
2023-05-13 01:35:08 +0900

    style: fix calendar outline style

--- src/components/main/calendar/Calendar.tsx
+++ src/components/main/calendar/Calendar.tsx
@@ -1,11 +1,9 @@
 import Calendar from 'react-calendar'
-import 'react-calendar/dist/Calendar.css'
 import * as S from './Calendar.style'
 
 const markDate = (date: Date) => {
-  // TODO: selected date array
-  const markedDate = new Date('2023-03-08')
-  if (markedDate.getDate() === date.getDate()) {
+  const markedDate = new Date('2023-05-02')
+  if (markedDate === date) {
     return 'highlight'
   }
 }
@@ -13,7 +11,10 @@ const markDate = (date: Date) => {
 const DiaryCalendar = () => {
   return (
     <S.CalendarWrapper>
-      <Calendar tileClassName={({ date }) => markDate(date) || 's'} />
+      <Calendar
+        goToRangeStartOnSelect={false}
+        tileClassName={({ date }) => markDate(date) || ''}
+      />
     </S.CalendarWrapper>
   )
 }
