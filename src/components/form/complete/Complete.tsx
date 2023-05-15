62175d5836d7f64a663e50f941e9c67ea94b1c42
Yewon <rkddp01@sookmyung.ac.kr>
2023-05-15 18:36:14 +0900

    fix: fix complete component

--- src/components/form/complete/Complete.tsx
+++ src/components/form/complete/Complete.tsx
@@ -1,14 +1,10 @@
 import * as S from './Complete.style'
+import ScoreGroup from 'components/common/score-group/ScoreGroup'
 
 const Complete = () => (
   <S.Container>
     <S.Title>일기 작성 완료🌟</S.Title>
-    <S.ScoreTab>
-      <S.ConTitle>아주 잘했어요!</S.ConTitle>
-      <S.ConA>80점</S.ConA>
-      <S.ConS1>쓰기:60점</S.ConS1>
-      <S.ConS2>말하기:100점</S.ConS2>
-    </S.ScoreTab>
+    <ScoreGroup writing={100} speaking={90} />
     <S.ToList>목록으로</S.ToList>
   </S.Container>
 )
