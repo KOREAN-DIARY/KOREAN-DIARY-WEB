dc8a02596aff8132811f72806dc64e27b7ab11e4
Yewon <rkddp01@sookmyung.ac.kr>
2023-05-15 18:44:36 +0900

    fix:  fix complete component style

--- src/components/form/complete/Complete.style.tsx
+++ src/components/form/complete/Complete.style.tsx
@@ -13,41 +13,6 @@ export const Title = styled.div`
   font-weight: bold;
   text-align: center;
 `
-export const ScoreTab = styled.div`
-  border: solid;
-  border-color: var(--grey);
-  width: 262px;
-  height: 176px;
-  flex-direction: column;
-  display: flex;
-  gap: 5px;
-`
-export const ConTitle = styled.div`
-  font-size: 18px;
-  font-weight: bold;
-  text-align: center;
-  color: var(--blue);
-  margin-top: 40px;
-`
-export const ConA = styled.div`
-  font-size: 18px;
-  color: var(--dark-grey);
-  text-align: center;
-  font-weight: bold;
-`
-export const ConS1 = styled.div`
-  font-size: 12px;
-  color: var(--dark-grey);
-  text-align: center;
-  font-weight: bold;
-  margin-top: 10px;
-`
-export const ConS2 = styled.div`
-  font-size: 12px;
-  color: var(--dark-grey);
-  font-weight: bold;
-  text-align: center;
-`
 export const ToList = styled.button`
   border: solid;
   border-radius: 20px;
