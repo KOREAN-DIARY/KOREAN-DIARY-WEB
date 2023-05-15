d1057ea6e178aa81fa1d66916a11edeb26efaeb0
chaerin <achrvvv@gmail.com>
2023-05-15 17:50:27 +0900

    chore: remove unused code

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosResponse, AxiosError, AxiosRequestConfig } from 'axios'
 import camelcaseKeys from 'camelcase-keys'
-import snakecaseKeys from 'snakecase-keys'
+// TODO: make snake case request
+// import snakecaseKeys from 'snakecase-keys'
 import queryString from 'query-string'
 
 export enum HttpStatusCode {
@@ -28,56 +29,6 @@ const getCamelCaseResponse = (responseConfig: AxiosResponse) => {
   const { data, ...rest } = responseConfig
   return { data: camelcaseKeys(data, { deep: true }), ...rest }
 }
-const getSnakeCaseRequest = (requestConfig: AxiosRequestConfig) => {
-  if (requestConfig.data) {
-    const { data, ...rest } = requestConfig
-    return { data: snakecaseKeys(data, { deep: true }), ...rest }
-  }
-  return requestConfig
-}
-
-// const setRequestConfig = (requestConfig: AxiosRequestConfig) => {
-//   const accessToken = Cookies.get(cookieName.accessToken)
-//   const snakeCaseConfig = getSnakeCaseRequest(requestConfig)
-//   if (accessToken) {
-//     snakeCaseConfig.headers = {
-//       ...snakeCaseConfig.headers,
-//       Authorization: `Bearer ${accessToken}`,
-//     }
-//   }
-//   return snakeCaseConfig
-// }
-
-// const setImgRequestConfig = (requestConfig: AxiosRequestConfig) => {
-//   const accessToken = Cookies.get(cookieName.accessToken)
-//   if (accessToken) {
-//     requestConfig.headers = {
-//       Authorization: `Bearer ${accessToken}`,
-//     }
-//   }
-//   return requestConfig
-// }
-
-// const handleError = async (error: AxiosError) => {
-//   if (
-//     error.response?.status === HttpStatusCode.Unauthorized &&
-//     error.config.url !== '/token'
-//   ) {
-//     try {
-//       const refreshToken = Cookies.get(cookieName.refreshToken)
-//       const { data } = await apiClient.post<{ accessToken: string }>(
-//         '/token/refresh',
-//         { refreshToken }
-//       )
-//       Cookies.set(cookieName.accessToken, data.accessToken)
-//       return error.config
-//     } catch (error) {
-//       logout()
-//       return Promise.reject(error)
-//     }
-//   }
-//   return Promise.reject(error)
-// }
 
 apiClient.interceptors.response.use(getCamelCaseResponse)
 apiClient.defaults.paramsSerializer = (params) => {
@@ -85,6 +36,5 @@ apiClient.defaults.paramsSerializer = (params) => {
 }
 
 voiceClient.interceptors.response.use(getCamelCaseResponse)
-// imgClient.interceptors.request.use(setImgRequestConfig)
 
 export default apiClient
