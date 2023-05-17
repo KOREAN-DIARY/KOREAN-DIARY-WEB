4ba463cdbe890d2a370f461275c0ac0a99ae3e3a
chaerin <achrvvv@gmail.com>
2023-05-17 16:16:28 +0900

    fix: fix app and index tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,44 @@
+import CompletePage from 'pages/Complete'
+import Diary from 'pages/Diary'
+import Form from 'pages/Form'
+import Login from 'pages/Login'
+import Main from 'pages/Main'
+import MyPage from 'pages/MyPage'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main />,
+  },
+  {
+    path: '/form',
+    element: <Form />,
+  },
+  {
+    path: '/my',
+    element: <MyPage />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/diary/:id',
+    element: <Diary />,
+  },
+  {
+    path: '/complete',
+    element: <CompletePage />,
+  },
+])
+
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  )
 }
 
 export default App
