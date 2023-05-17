ef30fa36f4b11e4085db03d4b9ac4fec0ae00f44
Yewon <rkddp01@sookmyung.ac.kr>
2023-05-17 13:58:31 +0900

    feat: implement diary detail layout

---
+++ src/components/diary/DiaryLayout.style.tsx
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled'
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+`
+export const DayFrame = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+export const Border = styled.div`
+  border-top: solid;
+  border-bottom: solid;
+  border-width: 0.125rem;
+  margin-top: 0.9375rem;
+  border-color: var(--grey);
+`
