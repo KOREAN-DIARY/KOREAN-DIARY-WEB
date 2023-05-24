c5135f24eab74e482e997287aaaaa952c5a10603
chaerin <achrvvv@gmail.com>
2023-05-24 15:31:26 +0900

    feat: add logo click handler

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Diary from 'pages/Diary'
-import Form from 'pages/Form'
-import Login from 'pages/Login'
-import Main from 'pages/Main'
-import MyPage from 'pages/MyPage'
 import 'styles/global.css'
-import CompletePage from 'pages/Complete'
 import { QueryClient, QueryClientProvider } from 'react-query'
 
 const queryClient = new QueryClient({
