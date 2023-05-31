b3f076fc08cf7bf3985a92bbb28ab364b4a8633c
chaerin <achrvvv@gmail.com>
2023-05-31 14:03:18 +0900

    feat: add use writing score mutaion

---
+++ src/hooks/query/useWritingScoreMutation.tsx
@@ -0,0 +1,42 @@
+import { audioClient } from 'api/index'
+import { ResponseType } from 'api/index'
+import { useMutation } from 'react-query'
+
+type ErrorInfo = {
+  help: string
+  originalString: string
+  correctWord: string
+}
+
+type WritingResponse = {
+  script: string
+  errorInfoList: ErrorInfo[]
+}
+
+const sendAudio = async (body: {
+  script: string
+}): Promise<WritingResponse> => {
+  try {
+    const { data } = await audioClient.post<ResponseType<WritingResponse>>(
+      '/writing',
+      body
+    )
+    return data.data
+  } catch (error) {
+    return Promise.reject()
+  }
+}
+
+export const useWritingScoreMutation = ({
+  onSuccess,
+  onError,
+}: {
+  onSuccess: (data: WritingResponse) => void
+  onError: (error: Error) => void
+}) => {
+  return useMutation('writing', sendAudio, {
+    retry: 5,
+    onSuccess,
+    onError,
+  })
+}
