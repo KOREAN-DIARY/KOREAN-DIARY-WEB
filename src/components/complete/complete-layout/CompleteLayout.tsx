a62069f8d6a33018532a88e343c87b9fc39ad14a
chaerin <achrvvv@gmail.com>
2023-06-06 21:52:46 +0900

    feat: add complete page score

--- src/components/complete/complete-layout/CompleteLayout.tsx
+++ src/components/complete/complete-layout/CompleteLayout.tsx
@@ -1,12 +1,18 @@
 import BaseLayout from 'components/common/base-layout/BaseLayout'
 import Complete from '../complete/Complete'
 import * as S from './CompleteLayout.style'
+import { useSearchParams } from 'react-router-dom'
+import queryString from 'query-string'
 
 const CompleteLayout = () => {
+  const [searchParams, setSearchParams] = useSearchParams()
+  const query = queryString.parse(searchParams.toString())
+  const { speaking, writing } = query
+
   return (
     <BaseLayout>
       <S.CompleteWrapper>
-        <Complete />
+        <Complete speaking={Number(speaking)} writing={Number(writing)} />
       </S.CompleteWrapper>
     </BaseLayout>
   )
