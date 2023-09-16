8d4b9c0d28d17fadcb2f7f0e2c1cefdac84d6a9b
SoleiI <kmssun0524@sookmyung.ac.kr>
2023-09-16 16:11:43 +0900

    fix: fix audio player size and button at step 4 page

--- src/components/form/audio-player/AudioPlayer.tsx
+++ src/components/form/audio-player/AudioPlayer.tsx
@@ -4,9 +4,10 @@ import { useState } from 'react'
 
 interface AudioPlayerProps {
   text: string
+  size: string
 }
 
-const AudioPlayer = ({ text }: AudioPlayerProps) => {
+const AudioPlayer = ({ text, size }: AudioPlayerProps) => {
   const [isPlaying, setIsPlaying] = useState(false)
   const playAudio = (text: string) => {
     speak(text, window.speechSynthesis)
@@ -25,7 +26,7 @@ const AudioPlayer = ({ text }: AudioPlayerProps) => {
 
   return (
     <button onClick={handleAudio}>
-      <S.Icon className="material-icons">
+      <S.Icon className="material-icons" style={{ fontSize: size }}>
         {isPlaying ? 'pause' : 'play_arrow'}
       </S.Icon>
     </button>
